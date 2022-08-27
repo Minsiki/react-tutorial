e51f0bf0c53aa385abf350b5a39f2e4beb250883
Minsiki <pro.mskim0215@gmail.com>
2022-08-27 18:01:47 +0900

    로그인/회원가입 템플릿 생성, 데이터 초기화 부분 확인 예정

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -8,23 +8,14 @@ import PostPage from './pages/PostPage';
 
 const App = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route element={<PostListPage />} path="/" exact />
-        <Route element={<PostListPage />} path="/@:username" />
-        <Route element={<LoginPage />} path="/login" />
-        <Route element={<RegisterPage />} path="/register" />
-        <Route element={<WritePage />} path="/write" />
-        <Route element={<PostPage />} path="/@:username/:postId" />
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<PostListPage />} path="/" exact />
+      <Route element={<PostListPage />} path="/@:username" />
+      <Route element={<LoginPage />} path="/login" />
+      <Route element={<RegisterPage />} path="/register" />
+      <Route element={<WritePage />} path="/write" />
+      <Route element={<PostPage />} path="/@:username/:postId" />
+    </Routes>
   );
 };
 
