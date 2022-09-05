9604e1c8fc848624bdbfbcb5e38f63835ed559d5
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-09-06 00:57:22 +0900

    editor 추가 및 스토어 연결

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,9 +1,13 @@
 import Responsive from '../components/common/Responsive';
-import BlogEditor from '../components/write/BlogEditor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
+import EditorContainer from '../containers/write/EditorContainer';
 const WritePage = () => {
   return (
     <Responsive>
-      <BlogEditor />
+      <EditorContainer />
+      <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
