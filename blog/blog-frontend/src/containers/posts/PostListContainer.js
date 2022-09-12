0f9473bb8d75dfdc8f29766f3a1d38c3e69bb48a
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-09-12 23:53:24 +0900

    게시글 불러오기 추가

---
+++ blog/blog-frontend/src/containers/posts/PostListContainer.js
@@ -0,0 +1,41 @@
+import qs from 'qs';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  useLocation,
+  useParams,
+} from '../../../node_modules/react-router-dom/index';
+import PostList from '../../components/posts/PostList';
+import { listPosts } from '../../modules/posts';
+
+const PostListContainer = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { username } = useParams();
+  const { posts, error, loading, user } = useSelector(
+    ({ posts, loading, user }) => ({
+      posts: posts.posts,
+      error: posts.error,
+      loading: loading['posts/LIST_POSTS'],
+      user: user.user,
+    }),
+  );
+
+  useEffect(() => {
+    const { tag, page } = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+    dispatch(listPosts({ tag, username, page }));
+  }, [dispatch, location.search]);
+
+  return (
+    <PostList
+      loading={loading}
+      error={error}
+      posts={posts}
+      showWriteButton={user}
+    />
+  );
+};
+
+export default PostListContainer;
--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -4,6 +4,7 @@ import auth, { authSaga } from './auth';
 import user, { userSaga } from './user';
 import write, { writeSaga } from './write';
 import post, { postSaga } from './post';
+import posts, { postsSaga } from './posts';
 import loading from './loading';
 
 const rootReducer = combineReducers({
@@ -12,10 +13,11 @@ const rootReducer = combineReducers({
   user,
   write,
   post,
+  posts,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga(), postSaga(), postsSaga()]);
 }
 
 export default rootReducer;
